79cca9230ca9714c3f16cdf30d14216ebc346778
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2023-07-12 15:43:28 +0900

    commit

---
+++ social-service/src/main/java/untitled/domain/Weather.java
@@ -0,0 +1,24 @@
+package untitled.domain;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
+
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Weather {
+
+    private Double degree;
+
+    private Double precipitation;
+
+    private Double humidity;
+
+    private Double wind;
+}
